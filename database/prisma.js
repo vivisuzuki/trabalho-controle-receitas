eb55453b3e8725aba3eef7fd12739969a8e92762
Viviane Suzuki <vivisuzuki@hotmail.com>
2023-06-05 23:23:35 -0300

    função de registro de usuário

---
+++ database/prisma.js
@@ -0,0 +1,3 @@
+const { PrismaClient } = require("@prisma/client");
+
+module.exports = new PrismaClient(); //exportando/iniciando o prisma client para o restante do projeto
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const z = require("zod"); //utilizando para validar usuário
+const bcrypt = require("bcrypt"); //utilizado para criptografar senhas
+const { findUserByEmail, saveUser } = require("../database/user");
 
 const router = express.Router();
 
@@ -9,17 +11,28 @@ const UserSchema = z.object({
     password: z.string().min(6)
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     try {
         const user = UserSchema.parse(req.body);
-        res.send({ user });
+        const isEmailAlreadyBeingUsed = await findUserByEmail(user.email);
+        if (isEmailAlreadyBeingUsed) return res.status(400).json({ //verificando se email já está sendo usado
+            message: "Email is already being used",
+        })
+
+        const hashedPassword = bcrypt.hashSync(user.password, 10); //criptografando senha
+        user.password = hashedPassword; //substituindo senha por senha criptografada
+
+        const savedUser = await saveUser(user); //salvando user no banco
+        delete savedUser.password; //excluindo senha antes de devolver na request
+
+        res.json({ user: savedUser });
     } catch (error) {
-        if (error instanceof z.ZodError) {
+        if (error instanceof z.ZodError) { //tratando erros de validações inseridas pra cadastro de usuários
             res.status(422).json({
                 message: error.errors,
             });
         }
-        res.status(500).json({
+        res.status(500).json({  //tratando demais erros
             message: "Server Error",
         });
     };
